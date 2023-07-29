c9e3d3c0d3dea433f96891cfd71d475398f6c76e
sehrishgill99 <140318895+sehrishgill99@users.noreply.github.com>
2023-07-29 21:27:47 +0500

    Add files via upload

---
+++ challenge6.js
@@ -0,0 +1,23 @@
+//Challenge 6
+
+const numberVariable = 0 ;
+
+numberVariable++;
+numberVariable++;
+numberVariable++;
+console.log(numberVariable);
+
+// it is wrong because const value can't be changed 
+
+let numberVariable = 0 ;
+undefined
+numberVariable++;
+0
+numberVariable++;
+1
+numberVariable++;
+2
+console.log(numberVariable);
+3 
+//numberVariable++; everytime this runs +1 is added into it 
+ 
---
+++ challenge7.js
@@ -0,0 +1,18 @@
+//challenge 7
+
+const firstNumber = 20;
+const secondNumber = '20';
+
+const result = firstNumber === secondNumber;
+
+console.log(result);
+
+const firstNumber1 = 20;
+const secondNumber1 = '20';
+
+const result1 = firstNumber == secondNumber;
+
+console.log(result);
+
+
+// first one is false and second one is true 
